a918f5620b79301a43e0a025b27852f098b4a47c
Akash Sharma <akash.sharma@o2h.com>
2021-03-07 08:56:52 +0530

    Login/Register Authentication, Route Authentication

--- client/src/utils/AuthRoute.js
+++ client/src/utils/AuthRoute.js
@@ -4,12 +4,11 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../context/auth'
 import { Route, Redirect } from 'react-router-dom'
 
-
 const AuthRoute = ( {component : Component, ...props} ) => {
     const { user } = useContext(AuthContext)
     return (
         <Route {...props} render={ props => user ? <Redirect to="/"/> : <Component {...props}/> }/>
     )
-}
+} 
 
 export default AuthRoute
